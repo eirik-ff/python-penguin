9b5a8e1092bcbfcbb68d09d943786419c965131a
Eirik Falck <eirik.falck@gmail.com>
2018-11-06 20:22:11 +0100

    enemies is a list

--- MyFirstPenguin/utilities.py
+++ MyFirstPenguin/utilities.py
@@ -49,7 +49,7 @@ def moveTowardsCenterOfMap(body):
 def chooseAction(body):
     action = PASS
     #action = moveTowardsCenterOfMap(body)
-    action = moveTowardsPoint(body, body["enemies"]["x"], body["enemies"]["y"])
+    action = moveTowardsPoint(body, body["enemies"][0]["x"], body["enemies"][0]["y"])
     return action
 
 def distanceToEnemyXandY(body, myPosX, myPosY, enPosX, enPosY):
