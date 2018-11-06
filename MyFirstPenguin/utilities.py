12a781235a254a1495d4f2bb5738c170fc8f69dc
Martin Falang <martfal@stud.ntnu.no>
2018-11-06 19:06:06 +0100

    changed layout

---
+++ MyFirstPenguin/utilities.py
@@ -0,0 +1,50 @@
+
+def doesCellContainWall(walls, x, y):
+    for wall in walls:
+        if wall["x"] == x and wall["y"] == y:
+            return True
+    return False
+
+def wallInFrontOfPenguin(body):
+    xValueToCheckForWall = body["you"]["x"]
+    yValueToCheckForWall = body["you"]["y"]
+    bodyDirection = body["you"]["direction"]
+
+    if bodyDirection == "top":
+        yValueToCheckForWall -= 1
+    elif bodyDirection == "bottom":
+        yValueToCheckForWall += 1
+    elif bodyDirection == "left":
+        xValueToCheckForWall -= 1
+    elif bodyDirection == "right":
+        xValueToCheckForWall += 1
+    return doesCellContainWall(body["walls"], xValueToCheckForWall, yValueToCheckForWall)
+
+def moveTowardsPoint(body, pointX, pointY):
+    penguinPositionX = body["you"]["x"]
+    penguinPositionY = body["you"]["y"]
+    plannedAction = PASS
+    bodyDirection = body["you"]["direction"]
+
+    if penguinPositionX < pointX:
+        plannedAction =  MOVE_RIGHT[bodyDirection]
+    elif penguinPositionX > pointX:
+        plannedAction = MOVE_LEFT[bodyDirection]
+    elif penguinPositionY < pointY:
+        plannedAction = MOVE_DOWN[bodyDirection]
+    elif penguinPositionY > pointY:
+        plannedAction = MOVE_UP[bodyDirection]
+
+    if plannedAction == ADVANCE and wallInFrontOfPenguin(body):
+        plannedAction = SHOOT
+    return plannedAction
+
+def moveTowardsCenterOfMap(body):
+    centerPointX = math.floor(body["mapWidth"] / 2)
+    centerPointY = math.floor(body["mapHeight"] / 2)
+    return moveTowardsPoint(body, centerPointX, centerPointY)
+
+def chooseAction(body):
+    action = PASS
+    action = moveTowardsCenterOfMap(body)
+    return action
