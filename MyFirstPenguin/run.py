dd2c7de67276c12569cf9679258768a49e095cb6
Martin Falang <martfal@stud.ntnu.no>
2018-11-06 20:06:03 +0100

    test

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -2,8 +2,71 @@ import os
 import json
 import random
 import math
-from utilities import *
+#from utilities import *
 from movement import *
+
+def doesCellContainWall(walls, x, y):
+    for wall in walls:
+        if wall["x"] == x and wall["y"] == y:
+            return True
+    return False
+
+def wallInFrontOfPenguin(body):
+    xValueToCheckForWall = body["you"]["x"]
+    yValueToCheckForWall = body["you"]["y"]
+    bodyDirection = body["you"]["direction"]
+
+    if bodyDirection == "top":
+        yValueToCheckForWall -= 1
+    elif bodyDirection == "bottom":
+        yValueToCheckForWall += 1
+    elif bodyDirection == "left":
+        xValueToCheckForWall -= 1
+    elif bodyDirection == "right":
+        xValueToCheckForWall += 1
+    return doesCellContainWall(body["walls"], xValueToCheckForWall, yValueToCheckForWall)
+
+def moveTowardsPoint(body, pointX, pointY):
+    penguinPositionX = body["you"]["x"]
+    penguinPositionY = body["you"]["y"]
+    plannedAction = PASS
+    bodyDirection = body["you"]["direction"]
+
+    if penguinPositionX < pointX:
+        plannedAction =  MOVE_RIGHT[bodyDirection]
+    elif penguinPositionX > pointX:
+        plannedAction = MOVE_LEFT[bodyDirection]
+    elif penguinPositionY < pointY:
+        plannedAction = MOVE_DOWN[bodyDirection]
+    elif penguinPositionY > pointY:
+        plannedAction = MOVE_UP[bodyDirection]
+
+    if plannedAction == ADVANCE and wallInFrontOfPenguin(body):
+        plannedAction = SHOOT
+    return plannedAction
+
+def moveTowardsCenterOfMap(body):
+    centerPointX = math.floor(body["mapWidth"] / 2)
+    centerPointY = math.floor(body["mapHeight"] / 2)
+    return moveTowardsPoint(body, centerPointX, centerPointY)
+
+def chooseAction(body):
+    action = PASS
+    action = moveTowardsCenterOfMap(body)
+    #action = moveTowardPoint(body, body["enemies"]["x"], body["enemies"]["y"])
+    return action
+
+def distanceToEnemyXandY(body, myPosX, myPosY, enPosX, enPosY):
+    distX = myPosX - enPosX
+    distY = myPosY - enPosY
+    return distX, distY
+
+def smallestDistanceToEnemy(body, myPosX, myPosY, enPosX, enPosY):
+    distX, distY = distanceToEnemyXandY(body, myPosX, myPosY, enPosX, enPosY)
+    if abs(distX) < abs(distY):
+        return distX
+    else:
+        return distY
 
 
 
