8114c1d0b89d866d4b5c26d28ec09bf4940ad558
Eirik Falck <eirik.falck@gmail.com>
2018-11-07 09:01:00 +0100

    mange småendringer nå, gidder ikke ny commit melding hver gang..

--- MyFirstPenguin/eirik.py
+++ MyFirstPenguin/eirik.py
@@ -29,7 +29,7 @@ def lookAtEnemy(body):
 
     dx = ex - x
     dy = ey - y
-    theta = atan2(dx, -dy) - pi/2
+    theta = atan2(dx, -dy) + pi/2
     print("x y ex ey theta: ", x, y, ex, ey, theta)
 
     if -pi / 4 <= theta <= pi / 4:
